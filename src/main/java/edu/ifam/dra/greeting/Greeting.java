5373567cecfbebadcf8b9c18e70b0788df541204
gabrieu33 <gabrielsouzaneves3@gmail.com>
2023-08-16 16:58:51 -0400

    initial commit

---
+++ src/main/java/edu/ifam/dra/greeting/Greeting.java
@@ -0,0 +1,28 @@
+package edu.ifam.dra.greeting;
+
+public class Greeting {
+	private long id;
+	private String nome;
+	
+	
+	public Greeting(long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
---
+++ src/test/java/edu/ifam/dra/greeting/GreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifam.dra.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
